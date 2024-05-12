b6385e7462b75d0fde6b4242ea70df1223252260
Shreyashi <shreyasi99rc@gmail.com>
2024-05-12 22:16:02 +0530

    Manually check all rows/cols for win scenario

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Player from "./components/Player.tsx";
 import GameBoard from "./components/GameBoard.tsx";
 import Log from "./components/Log.tsx";
 import GameOver from "./components/GameOver.tsx";
-import { WINNING_COMBINATIONS } from "./components/winning_combinations.js";
 
 const PLAYERS = {
   X: "Player",
@@ -40,35 +39,94 @@ function deriveGameBoard(gameTurns) {
   return gameBoard;
 }
 
-function deriveWinner(gameBoard, players) {
-  let winner;
-
-  for (const combination of WINNING_COMBINATIONS) {
-    const firstSquareSymbol =
-      gameBoard[combination[0].row][combination[0].column];
-    const secondSquareSymbol =
-      gameBoard[combination[1].row][combination[1].column];
-    const thirdSquareSymbol =
-      gameBoard[combination[2].row][combination[2].column];
-
-    if (
-      firstSquareSymbol &&
-      firstSquareSymbol === secondSquareSymbol &&
-      firstSquareSymbol === thirdSquareSymbol
-    ) {
-      winner = players[firstSquareSymbol];
-    }
+function isWinRowFor(gameBoard, rowIdx, player) {
+  if (player === PLAYERS.X) {
+    return (
+      gameBoard[rowIdx][0] === "X" &&
+      gameBoard[rowIdx][1] === "X" &&
+      gameBoard[rowIdx][2] === "X"
+    );
   }
-
-  return winner;
+  return (
+    gameBoard[rowIdx][0] === "O" &&
+    gameBoard[rowIdx][1] === "O" &&
+    gameBoard[rowIdx][2] === "O"
+  );
 }
 
+function isWinColFor(gameBoard, colIdx, player) {
+  if (player === PLAYERS.X) {
+    return (
+      gameBoard[0][colIdx] === "X" &&
+      gameBoard[1][colIdx] === "X" &&
+      gameBoard[2][colIdx] === "X"
+    );
+  }
+  return (
+    gameBoard[0][colIdx] === "O" &&
+    gameBoard[1][colIdx] === "O" &&
+    gameBoard[2][colIdx] === "O"
+  );
+}
+function isWinDiagFor(gameBoard, player) {
+  if (player === PLAYERS.X) {
+    return (
+      gameBoard[0][0] === "X" &&
+      gameBoard[1][1] === "X" &&
+      gameBoard[2][2] === "X"
+    );
+  }
+  return (
+    gameBoard[0][0] === "O" &&
+    gameBoard[1][1] === "O" &&
+    gameBoard[2][2] === "O"
+  );
+}
 
+function isWinDiag2For(gameBoard, player) {
+  if (player === PLAYERS.X) {
+    return (
+      gameBoard[0][2] === "X" &&
+      gameBoard[1][1] === "X" &&
+      gameBoard[2][0] === "X"
+    );
+  }
+  return (
+    gameBoard[0][2] === "O" &&
+    gameBoard[1][1] === "O" &&
+    gameBoard[2][0] === "O"
+  );
+}
+
+function deriveWinner(gameBoard, players) {
+  // check all rows
+  for (let i = 0; i < gameBoard.length; ++i) {
+    if (isWinRowFor(gameBoard, i, PLAYERS.X)) return PLAYERS.X;
+    if (isWinRowFor(gameBoard, i, PLAYERS.O)) return PLAYERS.O;
+  }
+
+  // check all cols
+  for (let j = 0; j < gameBoard[0].length; ++j) {
+    if (isWinColFor(gameBoard, j, PLAYERS.X)) return PLAYERS.X;
+    if (isWinColFor(gameBoard, j, PLAYERS.O)) return PLAYERS.O;
+  }
+
+  // check primary diag
+  if (isWinDiagFor(gameBoard, PLAYERS.X)) return PLAYERS.X;
+  if (isWinDiagFor(gameBoard, PLAYERS.O)) return PLAYERS.O;
+
+  // check secondary diag
+  if (isWinDiag2For(gameBoard, PLAYERS.X)) return PLAYERS.X;
+  if (isWinDiag2For(gameBoard, PLAYERS.O)) return PLAYERS.O;
+}
+
+// ignore square at [rowIdx][colIdx]
 // example returns = [[0,0], [0,2]]
-function getAllAvailableSquares(gameBoard) {
+function getAllAvailableSquares(gameBoard, rowIdx, colIdx) {
   let availbleSquares = [];
   for (let i = 0; i < gameBoard.length; ++i) {
     for (let j = 0; j < gameBoard[i].length; ++j) {
+      if (i === rowIdx && j === colIdx) continue;
       const sym = gameBoard[i][j];
       if (sym !== "O" && sym !== "X") {
         availbleSquares.push([i, j]);
@@ -92,14 +150,14 @@ function App() {
     setGameTurns((prevTurns) => {
       const currentPlayer = deriveActivePlayer(prevTurns);
 
-      if (currentPlayer === "X") {
+      if (currentPlayer === "X" && prevTurns.length !== 8) {
         // if it's bot's turn
-        const squares = getAllAvailableSquares(gameBoard);
+        const squares = getAllAvailableSquares(gameBoard, rowIndex, colIndex);
         const randSq = squares[Math.floor(Math.random() * squares.length)];
 
         // mark it as O
         return [
-          { square: { row: randSq[0], col: randSq[1] }, player: 'O' },
+          { square: { row: randSq[0], col: randSq[1] }, player: "O" },
           { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
           ...prevTurns,
         ];
